585d99c7fadc25a8f9ad793e6b4db7259a541d82
fabio guelfi <fabioguelfiunix@gmail.com>
2018-07-07 18:49:07 -0300

    Pipes for data transformation

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,6 +4,7 @@ interface Passager {
   id: number;
   fullname: string;
   checkIn: boolean;
+  checkInDate: number | null;
 }
 
 @Component({
@@ -16,17 +17,11 @@ interface Passager {
         <li *ngFor="let passager of passagers; let i = index;">
         <span class="status" [style.backgroundColor]="(passager.checkIn ? '#32e565' : '#e83e32')"></span>
         {{ i }}: {{ passager.fullname }}
-        </li>
-      </ul>
-      <br>
-      <h3>Airline passagers</h3>
-      <ul>
-        <li *ngFor="let passager of passagers; let i = index;">
-        <span class="status" 
-        [ngStyle]="{ 
-          backgroundColor: passager.checkIn ? '#32e565' : '#e83e32'
-         }"></span>
-        {{ i }}: {{ passager.fullname }}
+        <p>{{ passager | json }}</p>
+        <div class="data">
+          Check in date: 
+          {{ passager.checkInDate ? (passager.checkInDate | date: 'yMMMd') : 'Passager not checked' }}
+        </div>
         </li>
       </ul>
     </div>
@@ -37,22 +32,26 @@ export class AppComponent {
     {
       id: 1,
       fullname: "Fabio guelfi",
-      checkIn: true
+      checkIn: true,
+      checkInDate: 14916020300
     },
     {
       id: 2,
       fullname: "Renata guelfi",
-      checkIn: false
+      checkIn: false,
+      checkInDate: null
     },
     {
       id: 3,
       fullname: "Ana Julgia guelfi",
-      checkIn: true
+      checkIn: true,
+      checkInDate: 18916020300
     },
     {
       id: 4,
       fullname: "Ivar guelfi",
-      checkIn: false
+      checkIn: false,
+      checkInDate: 12916020300
     }
   ];
 }
