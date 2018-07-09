e95bfe04eb436457b3007fe628596cc37e5cea65
fabio guelfi <fabioguelfiunix@gmail.com>
2018-07-08 21:31:55 -0300

    Http put, delete with immutable state

--- app/passager-dashbord/containers/passager-dashboard/passager-dashbord.component.ts
+++ app/passager-dashbord/containers/passager-dashboard/passager-dashbord.component.ts
@@ -31,18 +31,37 @@ export class PassagerDashbordComponent implements OnInit {
   constructor(private passagerDashbordServices: PassagerDashbordServices) { }
 
   ngOnInit(): void {
-    this.passagerDashbordServices.getPassagers()
+    this.passagerDashbordServices
+      .getPassagers()
       .subscribe(
-        (res: Array<Passager>) => this.passagers = res,
+        (res: Array<Passager>) => (this.passagers = res),
         err => console.log(err)
-      )
-  }
-
-  public handleRemove(event): void {
-    console.log(event);
+      );
   }
 
   public handleEdit(event): void {
     console.log(event);
+    this.passagerDashbordServices
+      .updatePassager(event)
+      .subscribe((data: Passager) => {
+        this.passagers = this.passagers.map((passager: Passager) => {
+          if (passager.id === event.id) {
+            passager = Object.assign({}, passager, event);
+          }
+          return passager;
+        });
+      });
   }
+
+  public handleRemove(event): void {
+    console.log(event);
+    this.passagerDashbordServices.removePassager(event).subscribe(
+      (data: Passager) => {
+        this.passagers = this.passagers.filter((passager: Passager) => {
+          return passager.id !== event.id;
+        });
+      }
+    )
+  }
+
 }
--- app/passager-dashbord/services/passager-dashbord.service.ts
+++ app/passager-dashbord/services/passager-dashbord.service.ts
@@ -1,16 +1,29 @@
-import { Passager } from './../models/passager.interface';
-import { Inject, Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
+import { Passager } from "./../models/passager.interface";
+import { Inject, Injectable } from "@angular/core";
+import { Http, Response } from "@angular/http";
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/map";
 
-const PASSAGET_API: string = `/api/passagers`;
+const PASSAGER_API: string = `/api/passagers`;
 @Injectable()
 export class PassagerDashbordServices {
     constructor(private http: Http) { }
 
     public getPassagers(): Observable<Array<Passager>> {
-        return this.http.get(PASSAGET_API)
-        .map((response: Response) => response.json()) 
+        return this.http
+            .get(PASSAGER_API)
+            .map((response: Response) => response.json());
     }
-}   
+
+    public updatePassager(passager: Passager): Observable<Passager> {
+        return this.http
+            .put(PASSAGER_API + `/${passager.id}`, passager)
+            .map((response: Response) => response.json());
+    }
+
+    public removePassager(passager: Passager): Observable<Passager> {
+        return this.http
+            .delete(PASSAGER_API + `/${passager.id}`)
+            .map((response: Response) => response.json());
+    }
+}
